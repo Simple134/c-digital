83e7cedfa0cbb884bd07f014d3e7694bd282f987
Simple134 <sanchezjosue2001@gmail.com>
2025-02-10 22:14:15 -0400

    .

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,7 +1,6 @@
 "use client"
 import { useRouter, usePathname } from "next/navigation";
 import Link from "next/link";
-import Image from "next/image";
 import { Container } from "@bitnation-dev/components";
 
 const Header = () => {
