8777683ab2975ddd0d07da335138e642b07ae7ef
Simple134 <sanchezjosue2001@gmail.com>
2025-04-03 22:12:33 -0400

    .

--- src/components/ui/infinite-moving-card.tsx
+++ src/components/ui/infinite-moving-card.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { cn } from "@/app/lib/utils";
-import Image from "next/image";
 import React, { useEffect, useState, useCallback, useRef } from "react";
 
 export const InfiniteMovingCards = ({
